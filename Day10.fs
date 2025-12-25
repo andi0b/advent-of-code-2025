01b18a7a77a383dca888c35377d28b64d8e62740
andi0b <andix25@gmail.com>
2025-12-25 06:00:00 +0000

    Day10

--- Day10.fs
+++ Day10.fs
@@ -37,7 +37,7 @@ let parse =
 
     Array.map parseLine
 
-let allCombinations list =
+let inline allCombinations list =
     let rec comb k =
         function
         | _ when k = 0 -> [ [] ]
@@ -66,7 +66,6 @@ let part1 input =
 
     parse input |> Array.sumBy minButtonPresses
 
-
 let toRightAlignedVector (input: 'T[]) =
     let n = Vector<'T>.Count
 
@@ -81,36 +80,34 @@ let toRightAlignedVector (input: 'T[]) =
 
     Vector<'T>(tmp)
 
-let vecToArray (vec: Vector<'a>) =
-    let array = Array.zeroCreate Vector<'a>.Count
-    vec.CopyTo(array)
-    array
-
-let getPattern (vec: Vector<uint16>) =
+let getPattern (vec: inref<Vector<uint16>>) =
     let vec' = vec.AsVector256()
-    let lsb = Avx2.And(vec', Vector256<uint16>.One)
-    let sign = Avx2.ShiftLeftLogical(lsb, 15uy)
+    let sign = Avx2.ShiftLeftLogical(vec', 15uy)
     sign.AsSByte() |> Avx2.MoveMask
-
 
 let part2 =
     let minButtonPresses machine =
+
+        let offset = Vector<uint16>.Count - machine.jolts.Length
+
         let patterns =
             machine.buttons
             |> Seq.map (fun b ->
-                let array = Array.zeroCreate machine.jolts.Length
-
+                let tmp = Array.zeroCreate<uint16> Vector<uint16>.Count
                 for i in b do
-                    array[i] <- 1us
+                     tmp[offset + i] <- 1us
 
-                array |> toRightAlignedVector)
+                Vector<uint16>(tmp))
             |> Seq.toList
             |> allCombinations
             |> Seq.map (fun buttons ->
-                    let sum = buttons |> List.reduce (+)
-                    let count = buttons |> List.length
-                    let pattern = getPattern sum
-                    pattern, (sum, count))
+                let mutable sum = Vector<uint16>.Zero
+                for b in buttons do
+                    sum <- sum + b
+
+                let count = buttons |> List.length
+                let pattern = getPattern &sum
+                pattern, (sum, count))
             |> Seq.append (seq { 0, (Vector<uint16>.Zero, 0) })
             |> Seq.groupBy fst
             |> Seq.map (fun (k, v) -> KeyValuePair.Create(k, v |> Seq.map snd |> Seq.toArray))
@@ -118,24 +115,27 @@ let part2 =
 
         let rec solveStep target =
             if target = Vector<uint16>.Zero then
-                Some 0
+                ValueSome 0
             else
-                match patterns |> HashMap.tryFind (getPattern target) with
+                match patterns |> HashMap.tryFind (getPattern &target) with
                 | ValueSome pattern ->
-                    pattern
-                    |> Array.choose (fun (sum, count) ->
-                        if Vector.LessThanAny(target, sum) then
-                            None
-                        else
-                            let newTarget = (target - sum) / Vector<uint16>(2us)
-                            solveStep newTarget |> Option.map (fun x -> x * 2 + count))
-                    |> function
-                        | [||] -> None
-                        | array -> Some(Array.min array)
-                | ValueNone -> None
+                    seq {
+                        for sum, count in pattern do
+                            if Vector.GreaterThanOrEqualAll(target, sum) then
+                                match (target - sum) / Vector<uint16>(2us) |> solveStep with
+                                | ValueSome x -> yield x * 2 + count
+                                | _ -> ()
+                    }
+                    |> Seq.fold
+                        (fun s n ->
+                            match s with
+                            | ValueNone -> ValueSome n
+                            | ValueSome sm -> min sm n |> ValueSome)
+                        ValueNone
+                | ValueNone -> ValueNone
 
         let target = machine.jolts |> Array.map uint16 |> toRightAlignedVector
-        solveStep target |> Option.defaultValue 100000
+        solveStep target |> ValueOption.get
 
     parse >> Array.sumBy minButtonPresses
 
